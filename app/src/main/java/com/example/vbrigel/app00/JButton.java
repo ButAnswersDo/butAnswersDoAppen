c3d3a7f56c53795945c636fa168159cf09dcc37f
Olof Wireklint <olofwi@student.chalmers.se>
2016-04-20 15:48:29 +0200

    La till en komentar

--- app/src/main/java/com/example/vbrigel/app00/JButton.java
+++ app/src/main/java/com/example/vbrigel/app00/JButton.java
@@ -16,4 +16,5 @@ public class JButton extends AppCompatActivity {
     public void onBusDoor(View view){
         Toast.makeText(getApplicationContext(),"Bekräftat: Det är fel på dörren", Toast.LENGTH_LONG).show();
     }
+    //test2
 }
