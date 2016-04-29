a3f95e453d6cffd87a305163b9da5a4ef7704680
gbgvinnie <brigel@student.chalmers.se>
2016-04-29 10:57:49 +0200

    Länk till databas tillagd

--- app/src/main/java/com/example/vbrigel/app00/MainActivity.java
+++ app/src/main/java/com/example/vbrigel/app00/MainActivity.java
@@ -5,12 +5,14 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 
 import com.example.vbrigel.app00.databinding.ActivityMainBinding;
+import com.firebase.client.Firebase;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         MainActivityHandlers handler = new MainActivityHandlers();
         binding.setHandler(handler);
