d34b9ef87ca2736104e3d62b4d0df5b942e132e5
Vincent <brigel@student.chalmers.se>
2016-05-11 21:32:41 +0200

    TestAgain
    Checking if the  Github application works properly now.

--- app/src/main/java/com/example/vbrigel/app00/LoginScreen.java
+++ app/src/main/java/com/example/vbrigel/app00/LoginScreen.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Spinner;
-
+//Test
 
 public class LoginScreen extends AppCompatActivity {
 
