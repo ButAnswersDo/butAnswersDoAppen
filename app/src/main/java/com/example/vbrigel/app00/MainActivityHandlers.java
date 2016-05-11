20fc8c07730cbbda1603e2c5ba56a36893b93f66
gbgvinnie <brigel@student.chalmers.se>
2016-05-11 17:00:47 +0200

    Added commentary,

---
+++ app/src/main/java/com/example/vbrigel/app00/MainActivityHandlers.java
@@ -0,0 +1,28 @@
+package com.example.vbrigel.app00;
+
+/**
+ * Created by Anton on 2016-04-27.
+ */
+
+import android.content.Context;
+import android.content.Intent;
+import android.databinding.BaseObservable;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivityHandlers extends BaseObservable{
+    private Context contextdoor;
+    private Context contextwhichdoor;
+    public void onClickDoorButton(View view) {
+        contextdoor = view.getContext();
+        Intent nextDoorPage = new Intent (contextdoor, SecondDoorPage.class);
+        contextdoor.startActivity(nextDoorPage);
+        }
+
+    public void onClickWhichDoorButton (View view){
+        contextwhichdoor = view.getContext();
+        Toast.makeText(contextwhichdoor, "Bekräftat, något är fel med bakre dörr", Toast.LENGTH_LONG).show();
+    }
+
+}
+
--- app/src/test/java/com/example/vbrigel/app00/UnitTest.java
+++ app/src/test/java/com/example/vbrigel/app00/UnitTest.java
@@ -7,10 +7,13 @@ import static org.junit.Assert.*;
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest {
+public class UnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+
+
     //Ser du det här?
 }
